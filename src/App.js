07383861d4e7e3a28917a7021cc7163632d395ca
Recep Terzi <recepterzi-67@hotmail.com>
2023-01-19 00:46:18 +0300

    update

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Home from "./components/Home/Home";
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </div>
     </>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,41 @@
 import React from "react";
 import "./Home.Module.css";
+import image1 from "../../assets/illustration-working.svg";
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <navbar id="navbar-section">
+        <div className="navbar-left">
+          <div className="navbar-logo">Shortly</div>
+          <div className="navbar-item">
+            <div className="item">Features</div>
+            <div className="item">Pricing</div>
+            <div className="item">Resources</div>
+          </div>
+        </div>
+        <div className="navbar-right">
+          <div className="navbar-right-item">
+            <div className="item">Login</div>
+            <button className="navbar-button">Sign Up</button>
+          </div>
+        </div>
+      </navbar>
+
+      <main id="main-section">
+        <div className="main-section-left">
+          <h1>More than just shorter links</h1>
+          <p>
+            Build your brand’s recognition and get detailed insights on how your
+            links are performing.
+          </p>
+          <button className="main-section-button">Get Started</button>
+        </div>
+        <div className="main-section-right">
+          <img src={image1} alt="" />
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Home;
